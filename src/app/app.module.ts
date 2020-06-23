2898b553b5bd6411aff1aa8d73c53869536eec3c
AHMarcos <marcos.alarcon@vallegrande.edu.pe>
2020-06-23 18:48:20 -0500

    Agregando rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// RUTAS
 import { AppRoutingModule } from './app-routing.module';
+
+// COMPONENTES
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +18,8 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
     HeaderComponent,
     FooterComponent,
     PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
   ],
   imports: [
     BrowserModule,
